1bd9aebb2710b8cf7ca230e5f58f7934daea1605
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-08-12 23:12:34 +0300

    upd NotesPage add enable generateMetadata (prev commit fixed it) details: client and server pages are no longer coupled thus server page is no longer treated as client by next js

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -6,42 +6,37 @@ import {
 } from "@tanstack/react-query";
 import NotesPageClient from "./Notes.client";
 import { APP_NOTES_FILTER_SLUG_PARAMS_INDEXES } from "@/lib/const";
-// import { Metadata } from "next";
+import { Metadata } from "next";
 
 interface NotesPageProps {
   params: Promise<{ slug: string[] }>;
 }
 
-// // todo?
-// // Next does not allow `export async function generateMetadata()` or `export const metadata`
-// // is this a bug?
-// // but `app/notes/[id]/page.tsx` works OK
+export async function generateMetadata({
+  params,
+}: NotesPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const tagName = slug[0];
 
-// export async function generateMetadata({
-//   params,
-// }: NotesPageProps): Promise<Metadata> {
-//   const { slug } = await params;
-//   const tagName = slug[0] ?? "";
-
-//   const metadata: Metadata = {
-//     title: `${tagName} notes -  NoteHub`,
-//     description: `${tagName} notes at NoteHub (a simple and efficient personal notes manager)`,
-//     openGraph: {
-//       title: `${tagName} notes -  NoteHub`,
-//       description: `${tagName} notes at NoteHub (a simple and efficient personal notes manager)`,
-//       url: "", // todo: after deploy
-//       images: [
-//         {
-//           url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
-//           width: "1471",
-//           height: "980",
-//           alt: "NoteHub Logo",
-//         },
-//       ],
-//     },
-//   };
-//   return metadata;
-// }
+  const metadata: Metadata = {
+    title: `${tagName} notes -  NoteHub`,
+    description: `${tagName} notes at NoteHub (a simple and efficient personal notes manager)`,
+    openGraph: {
+      title: `${tagName} notes -  NoteHub`,
+      description: `${tagName} notes at NoteHub (a simple and efficient personal notes manager)`,
+      url: "", // todo: after deploy
+      images: [
+        {
+          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+          width: "1471",
+          height: "980",
+          alt: "NoteHub Logo",
+        },
+      ],
+    },
+  };
+  return metadata;
+}
 
 export default async function NotesPage({ params }: NotesPageProps) {
   const queryClient = new QueryClient();
