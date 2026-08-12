a5fd21353475e555f094f3cc12c56cd2b96483df
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-08-12 22:30:42 +0300

    upd NoteDetails  add generateMetadata

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -5,10 +5,37 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 import NoteDetailsClient from "./NoteDetails.client";
+import { Metadata } from "next";
 
 type NoteDetailsProps = {
   params: Promise<{ id: string }>;
 };
+
+export async function generateMetadata({
+  params,
+}: NoteDetailsProps): Promise<Metadata> {
+  const { id } = await params;
+  const note = await fetchNoteById(id);
+
+  const metadata: Metadata = {
+    title: `${note.title} -  NoteHub`,
+    description: `${note.content} -  NoteHub`,
+    openGraph: {
+      title: `${note.title} -  NoteHub`,
+      description: `${note.content} -  NoteHub`,
+      url: "", // todo: after deploy
+      images: [
+        {
+          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+          width: "1471",
+          height: "980",
+          alt: "NoteHub Logo",
+        },
+      ],
+    },
+  };
+  return metadata;
+}
 
 export default async function NoteDetails({ params }: NoteDetailsProps) {
   const { id } = await params;
