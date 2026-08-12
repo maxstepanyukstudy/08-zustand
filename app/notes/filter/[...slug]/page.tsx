03f878c2466c553ecb7e88f207de3b647be7bb5a
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-08-12 22:58:05 +0300

    upd NotesPage add (disabled)  generateMetadata (because it is somehow broken)

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -5,6 +5,7 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 import NotesPageClient from "./Notes.client";
+// import { Metadata } from "next";
 
 // todo? move to util
 export const PARAMS_INDEX = {
@@ -14,6 +15,37 @@ export const PARAMS_INDEX = {
 interface NotesPageProps {
   params: Promise<{ slug: string[] }>;
 }
+
+// // todo?
+// // Next does not allow `export async function generateMetadata()` or `export const metadata`
+// // is this a bug?
+// // but `app/notes/[id]/page.tsx` works OK
+
+// export async function generateMetadata({
+//   params,
+// }: NotesPageProps): Promise<Metadata> {
+//   const { slug } = await params;
+//   const tagName = slug[0] ?? "";
+
+//   const metadata: Metadata = {
+//     title: `${tagName} notes -  NoteHub`,
+//     description: `${tagName} notes at NoteHub (a simple and efficient personal notes manager)`,
+//     openGraph: {
+//       title: `${tagName} notes -  NoteHub`,
+//       description: `${tagName} notes at NoteHub (a simple and efficient personal notes manager)`,
+//       url: "", // todo: after deploy
+//       images: [
+//         {
+//           url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+//           width: "1471",
+//           height: "980",
+//           alt: "NoteHub Logo",
+//         },
+//       ],
+//     },
+//   };
+//   return metadata;
+// }
 
 export default async function NotesPage({ params }: NotesPageProps) {
   const queryClient = new QueryClient();
