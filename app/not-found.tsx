c2d5db89f0164e65f38995610002b240d63d6dcb
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-08-13 18:17:52 +0300

    fix og images - number instead of string

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -11,8 +11,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
-        width: "1471",
-        height: "980",
+        width: 1471,
+        height: 980,
         alt: "NoteHub Logo",
       },
     ],
