68a30f31d0ec0f8a4cd8385eca7b2024bcd17a48
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-08-13 14:37:00 +0300

    create-form modal to page migration - upd NoteForm component - Formik to regular form

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -1,30 +1,13 @@
-import { ErrorMessage, Field, Form, Formik, type FormikHelpers } from "formik";
-import * as Yup from "yup";
+"use client";
 
 import css from "./NoteForm.module.css";
-import type { CreateNote, NoteTag } from "../../types/note";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { createNote } from "@/lib/api";
-import { validTags } from "@/lib/const";
+import { CreateNote, NoteTag } from "@/types/note";
+import Link from "next/link";
 
-const initialValues: CreateNote = {
-  title: "",
-  content: "",
-  tag: "Todo",
-};
-
-const validationSchema = Yup.object().shape({
-  title: Yup.string().min(3).max(50).required(),
-  content: Yup.string().max(500),
-  tag: Yup.string().oneOf(validTags).required(),
-});
-
-interface NoteFormProps {
-  onCancel: () => void;
-}
-
-export default function NoteForm({ onCancel }: NoteFormProps) {
+export default function NoteForm() {
   const queryClient = useQueryClient();
 
   const { mutate, isPending } = useMutation({
@@ -33,7 +16,6 @@ export default function NoteForm({ onCancel }: NoteFormProps) {
       queryClient.invalidateQueries({
         queryKey: ["notes"],
       });
-      onCancel();
     },
     onError: (e) => {
       const msg = "Error creating note";
@@ -42,94 +24,54 @@ export default function NoteForm({ onCancel }: NoteFormProps) {
     },
   });
 
-  const handleSubmit = (
-    formValues: CreateNote,
-    actions: FormikHelpers<CreateNote>,
-  ) => {
-    mutate(formValues, {
-      onSuccess: () => {
-        actions.resetForm();
-      },
+  function handleSubmit(formData: FormData) {
+    const note: CreateNote = {
+      title: formData.get("title") as string,
+      content: formData.get("content") as string,
+      tag: formData.get("tag") as NoteTag,
+    };
+
+    mutate(note, {
+      onSuccess: () => toast("Note created"),
     });
-  };
+  }
 
   return (
-    <>
-      <Formik
-        initialValues={initialValues}
-        onSubmit={handleSubmit}
-        validationSchema={validationSchema}
-      >
-        {({ isValid, dirty }) => {
-          return (
-            <Form className={css.form}>
-              <div className={css.formGroup}>
-                <label htmlFor="title">Title</label>
-                <Field
-                  id="title"
-                  type="text"
-                  name="title"
-                  className={css.input}
-                />
-                <ErrorMessage
-                  component="span"
-                  name="title"
-                  className={css.error}
-                />
-              </div>
+    <form action={handleSubmit} className={css.form}>
+      <div className={css.formGroup}>
+        <label htmlFor="title">Title</label>
+        <input id="title" type="text" name="title" className={css.input} />
+      </div>
 
-              <div className={css.formGroup}>
-                <label htmlFor="content">Content</label>
-                <Field
-                  as="textarea"
-                  id="content"
-                  name="content"
-                  rows={8}
-                  className={css.textarea}
-                />
-                <ErrorMessage
-                  component="span"
-                  name="content"
-                  className={css.error}
-                />
-              </div>
+      <div className={css.formGroup}>
+        <label htmlFor="content">Content</label>
+        <textarea
+          id="content"
+          name="content"
+          rows={8}
+          className={css.textarea}
+        ></textarea>
+      </div>
 
-              <div className={css.formGroup}>
-                <label htmlFor="tag">Tag</label>
-                <Field as="select" id="tag" name="tag" className={css.select}>
-                  <option value="Todo">Todo</option>
-                  <option value="Work">Work</option>
-                  <option value="Personal">Personal</option>
-                  <option value="Meeting">Meeting</option>
-                  <option value="Shopping">Shopping</option>
-                </Field>
-                <ErrorMessage
-                  component="span"
-                  name="tag"
-                  className={css.error}
-                />
-              </div>
+      <div className={css.formGroup}>
+        <label htmlFor="tag">Tag</label>
+        <select id="tag" name="tag" className={css.select}>
+          <option value="Todo">Todo</option>
+          <option value="Work">Work</option>
+          <option value="Personal">Personal</option>
+          <option value="Meeting">Meeting</option>
+          <option value="Shopping">Shopping</option>
+        </select>
+      </div>
 
-              <div className={css.actions}>
-                <button
-                  type="button"
-                  className={css.cancelButton}
-                  onClick={onCancel}
-                >
-                  Cancel
-                </button>
-                <button
-                  type="submit"
-                  className={css.submitButton}
-                  disabled={!isValid || !dirty || isPending}
-                >
-                  {isPending ? "Creating..." : "Create note"}
-                </button>
-              </div>
-            </Form>
-          );
-        }}
-      </Formik>
-    </>
+      <div className={css.actions}>
+        <Link className={css.cancelButton} href="/notes/filter/all">
+          Cancel
+        </Link>
+        <button type="submit" className={css.submitButton} disabled={isPending}>
+          {isPending ? "Creating..." : "Create note"}
+        </button>
+      </div>
+    </form>
   );
 }
